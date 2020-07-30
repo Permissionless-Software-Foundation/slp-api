91757650c5eb5b0c6050e0996e078d14b2e665d1
Chris Troutner <chris.troutner@gmail.com>
2020-07-30 13:16:01 -0700

    fix(semantic release): Making sure semantic release works

--- config/env/common.js
+++ config/env/common.js
@@ -6,7 +6,14 @@
 module.exports = {
   port: process.env.PORT || 5001,
   logPass: 'test',
+
+  // Email Server, for sending emails.
   emailServer: process.env.EMAILSERVER ? process.env.EMAILSERVER : 'mail.someserver.com',
   emailUser: process.env.EMAILUSER ? process.env.EMAILUSER : 'noreply@someserver.com',
-  emailPassword: process.env.EMAILPASS ? process.env.EMAILPASS : 'emailpassword'
+  emailPassword: process.env.EMAILPASS ? process.env.EMAILPASS : 'emailpassword',
+
+  // BCH Full Node RPC
+  rpcUserName: process.env.RPC_USER_NAME ? process.env.RPC_USER_NAME : 'bitcoin',
+  rpcPassword: process.env.RPC_PASSWORD ? process.env.RPC_PASSWORD : 'password',
+  rpcUrl: process.env.RPC_URL ? process.env.RPC_URL : '0.0.0.0:8332'
 }
