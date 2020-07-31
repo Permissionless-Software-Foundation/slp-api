e6eecabf04c53f44203d7462acc6876a00427c51
Chris Troutner <chris.troutner@gmail.com>
2020-07-30 18:15:39 -0700

    Stubbing isValidSlpTxid

--- test/a05-slp.spec.js
+++ test/a05-slp.spec.js
@@ -50,8 +50,9 @@ describe('SLP', () => {
 
       // Mock the actual network call.
       if (process.env.TEST === 'unit') {
-        sandbox.stub(slp.rpc, 'getRawTransaction').resolves(mockTx)
+        // sandbox.stub(slp.rpc, 'getRawTransaction').resolves(mockTx)
         sandbox.stub(slp.slpValidator, 'getRawTransaction').resolves(mockTx)
+        sandbox.stub(slp.slpValidator, 'isValidSlpTxid').resolves(true)
       }
 
       const txid =
@@ -72,7 +73,7 @@ describe('SLP', () => {
       // Mock the actual network call for unit tests.
       if (process.env.TEST === 'unit') {
         sandbox.stub(slp.rpc, 'getRawTransaction').resolves(mockTx)
-        sandbox.stub(slp.slpValidator, 'getRawTransaction').resolves(mockTx)
+        sandbox.stub(slp.slpValidator, 'isValidSlpTxid').resolves(false)
       }
 
       const txid =
